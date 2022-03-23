e6984cbc03a79f65fb9e444b27b691608ac63820
Jeremy Johnson <jeremy.m.johnson@davenportiowa.com>
2022-03-23 07:52:28 -0500

    now importing from /imports folder

---
+++ imports/Enable-WindowsStore.ps1
@@ -0,0 +1,3 @@
+# Windows Store
+#Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsStore -Name RemoveWindowsStore -Value 0
+#Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\AllowStore -Name DoNotAllow -Value 0
--- logon.ps1
+++ logon.ps1
@@ -1,5 +1,4 @@
-# Windows Store
-#Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsStore -Name RemoveWindowsStore -Value 0
-#Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\AllowStore -Name DoNotAllow -Value 0
+. .\imports\Disable-UAC.ps1
+. .\imports\Enable-WindowsStore.ps1
 
 exit

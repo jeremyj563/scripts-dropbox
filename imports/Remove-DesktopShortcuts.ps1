cf2b120c15b1f575c943e382438597cf1d872e68
Jeremy Johnson <jeremy.m.johnson@davenportiowa.com>
2022-03-23 07:58:46 -0500

    bug fix in Remove-DesktopShortcuts.ps1

--- imports/Remove-DesktopShortcuts.ps1
+++ imports/Remove-DesktopShortcuts.ps1
@@ -1,3 +1,3 @@
-Remove-Item -Path "$env:USERPROFILE\Desktop\HelpDesk.url"
+Remove-Item -Path "$env:USERPROFILE\Desktop\HelpDesk.url" -Force -ErrorAction Ignore
 Remove-Item -Path "$env:USERPROFILE\Desktop\Munis Dashboard Production.url" -Force -ErrorAction Ignore
 Remove-Item -Path "$env:USERPROFILE\Desktop\Remote Assist.lnk" -Force -ErrorAction Ignore
